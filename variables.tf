e48d12ff5dc936f8e7db976e3988cf2badc414f8
Yara-Khedr <yaraadelkhedr214@gmail.com>
2026-08-07 21:44:30 +0300

    Add compartment_id variable

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "compartment_id" {
+  description = "OCID of my compartment where resources will be created (intern-17-yara-khedr-cmp)"
+  type        = string
+  default     = "ocid1.compartment.oc1..aaaaaaaauhqrwvwzpavy2cuiayyinuryeppiplca5yctnpiomlfrivwirnna"
+}
