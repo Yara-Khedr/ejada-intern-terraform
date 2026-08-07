438b261e7a0ab1eb07a286d9386a5fd997cb3d47
Yara-Khedr <yaraadelkhedr214@gmail.com>
2026-08-07 22:35:50 +0300

    Add IGW

--- network.tf
+++ network.tf
@@ -3,3 +3,12 @@ resource "oci_core_vcn" "YaraVCN01" {
   cidr_block     = "10.0.0.0/16"
   display_name   = "YaraVCN01"
 }
+
+resource "oci_core_internet_gateway" "YaraIGW01" {
+    #Required
+    compartment_id = var.compartment_id
+    vcn_id = oci_core_vcn.YaraVCN01.id
+
+    enabled = true
+    display_name = "YaraIGW01"
+}
