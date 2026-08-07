7b7638398a313a1254e2b9ec2cdd6d16695e835b
Yara-Khedr <yaraadelkhedr214@gmail.com>
2026-08-08 00:43:44 +0300

    Add file storage system, mount target, and export

--- storage.tf
+++ storage.tf
@@ -14,3 +14,28 @@ resource "oci_core_volume_attachment" "YaraBVAttach01" {
     volume_id = oci_core_volume.YaraBlockVolume01.id
     display_name = "YaraBVAttach01"
 }
+
+resource "oci_file_storage_file_system" "YaraFs01" {
+
+    availability_domain = "oXVt:ME-JEDDAH-1-AD-1"
+    compartment_id = var.compartment_id
+    display_name = "YaraFs01"
+
+}
+
+resource "oci_file_storage_mount_target" "YaraMT01" {
+
+    availability_domain = "oXVt:ME-JEDDAH-1-AD-1"
+    compartment_id = var.compartment_id
+    subnet_id = oci_core_subnet.YaraSubnet_Pub01.id
+    display_name = "YaraMT01"
+
+}
+
+resource "oci_file_storage_export" "YaraExp01" {
+
+    export_set_id = oci_file_storage_mount_target.YaraMT01.export_set_id
+    file_system_id = oci_file_storage_file_system.YaraFs01.id
+    path = "/yara-fs"
+
+}
