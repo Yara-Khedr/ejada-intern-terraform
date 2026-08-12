5293adea3874d1b6a5a77f70e40f6b6a633cfe38
Yara-Khedr <yaraadelkhedr214@gmail.com>
2026-08-12 22:45:54 +0300

    W2: create route tables and attach to subnets

--- week2/locals.tf
+++ week2/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  all_services_id = data.oci_core_services.all_services.services[0].id
+  all_services_id   = data.oci_core_services.all_services.services[0].id
+  all_Services_cidr = data.oci_core_services.all_services.services[0].cidr_block
 }
--- week2/main.tf
+++ week2/main.tf
@@ -13,6 +13,7 @@ resource "oci_core_subnet" "YaraSubnet_Pub02" {
   cidr_block                 = "10.0.1.0/24"
   display_name               = "YaraSubnet_Pub02"
   prohibit_public_ip_on_vnic = false
+  route_table_id             = oci_core_route_table.YaraRT_Pub02.id
 }
 
 resource "oci_core_subnet" "YaraSubnet_Priv02" {
@@ -22,6 +23,7 @@ resource "oci_core_subnet" "YaraSubnet_Priv02" {
   cidr_block                 = "10.0.2.0/24"
   display_name               = "YaraSubnet_Priv02"
   prohibit_public_ip_on_vnic = true
+  route_table_id             = oci_core_route_table.YaraRT_Priv02.id
 }
 
 resource "oci_core_internet_gateway" "YaraIGW02" {
@@ -54,4 +56,33 @@ resource "oci_core_service_gateway" "YaraServiceGW02" {
 
   vcn_id       = oci_core_vcn.YaraVCN02.id
   display_name = "YaraServiceGW02"
+}
+
+resource "oci_core_route_table" "YaraRT_Pub02" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.YaraVCN02.id
+  display_name   = "YaraRT_Pub02"
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.YaraIGW02.id
+  }
+}
+
+
+resource "oci_core_route_table" "YaraRT_Priv02" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.YaraVCN02.id
+  display_name   = "YaraRT_Priv02"
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.YaraNATGW02.id
+  }
+
+  route_rules {
+    destination       = local.all_Services_cidr
+    destination_type  = "SERVICE_CIDR_BLOCK"
+    network_entity_id = oci_core_service_gateway.YaraServiceGW02.id
+  }
 }
