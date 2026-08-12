50c8bf19a595346f1cf385989af0d828b8d50d22
Yara-Khedr <yaraadelkhedr214@gmail.com>
2026-08-12 21:12:34 +0300

    W2: create public and private subnets

--- week2/main.tf
+++ week2/main.tf
@@ -5,3 +5,21 @@ resource "oci_core_vcn" "YaraVCN02" {
   display_name   = "YaraVCN02"
 
 }
+
+resource "oci_core_subnet" "YaraSubnet_Pub02" {
+
+  compartment_id             = var.compartment_id
+  vcn_id                     = oci_core_vcn.YaraVCN02.id
+  cidr_block                 = "10.0.1.0/24"
+  display_name               = "YaraSubnet_Pub02"
+  prohibit_public_ip_on_vnic = false
+}
+
+resource "oci_core_subnet" "YaraSubnet_Priv02" {
+
+  compartment_id             = var.compartment_id
+  vcn_id                     = oci_core_vcn.YaraVCN02.id
+  cidr_block                 = "10.0.2.0/24"
+  display_name               = "YaraSubnet_Priv02"
+  prohibit_public_ip_on_vnic = true
+}
